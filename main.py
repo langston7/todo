c9dde9fd318e5ff38dfee6162c30ff0f8d114a54
langston7 <ericmahoney137@gmail.com>
2020-11-18 19:54:04 -0500

    11/18/20 first working

--- main.py
+++ main.py
@@ -1,16 +1,100 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+import sqlite3
+from sqlite3 import Error
 
 
-# Press the green button in the gutter to run the script.
+def create_connection(dbFile):
+    conn = None
+    try:
+        conn = sqlite3.connect(dbFile)
+    except Error as e:
+        print(e)
+    return conn
+
+
+def new_task(conn, task):
+    sql = ''' INSERT INTO tasks(desc, complete)
+                  VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+    return cur.lastrowid
+
+
+def update_task(conn, task):
+    sql = ''' UPDATE tasks
+              SET desc = ?
+              WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+
+
+def view_tasks(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def delete_task(conn, taskID):
+    # Should probably reset taskIDs?  Not sure if that's a smart move
+    sql = 'DELETE FROM tasks WHERE id=?'
+    cur = conn.cursor()
+    cur.execute(sql, taskID)
+    conn.commit()
+
+
+def mark_task_as_done(conn, idAndText):
+    sql = ''' UPDATE tasks
+                  SET complete = ?
+                  WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, idAndText)
+    conn.commit()
+
+
+def main():
+    database = r"C:\sqlite\db\todo.db"
+    conn = create_connection(database)
+
+    script = sys.argv[0]
+    if len(sys.argv) == 1:  # no arguments, so print help message
+        print("""Commands are: --new "description", --view, --edit "taskID"
+         "description", --delete "taskID", --mark taskID""")
+        return
+    action = sys.argv[1]
+
+    if action == '--new':
+        # By default, set complete column to incomplete
+        desc = sys.argv[2]
+        task = (desc, 'Incomplete')
+        new_task(conn, task)       # ???
+
+    elif action == '--view':
+        # Print rows
+        view_tasks(conn)
+
+    elif action == '--edit':
+        # Take ID to edit and new description
+        taskID = sys.argv[2]
+        desc = sys.argv[3]
+        update_task(conn, (desc, taskID,))
+
+    elif action == '--delete':
+        taskID = sys.argv[2]
+        delete_task(conn, (taskID,))
+
+    elif action == '--mark':
+        # Create markText variable because I don't know how else to create the tuple
+        taskID = sys.argv[2]
+        markText = "Done"
+        mark_task_as_done(conn, (markText, taskID))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
