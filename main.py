464fa09505dccc8eb487c8a74d0625647910c7a9
langston7 <ericmahoney137@gmail.com>
2020-11-22 14:38:15 -0500

    11/22/20

--- main.py
+++ main.py
@@ -1,65 +1,10 @@
 import sys
-import sqlite3
-from sqlite3 import Error
-
-
-def create_connection(dbFile):
-    conn = None
-    try:
-        conn = sqlite3.connect(dbFile)
-    except Error as e:
-        print(e)
-    return conn
-
-
-def new_task(conn, task):
-    sql = ''' INSERT INTO tasks(desc, complete)
-                  VALUES(?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, task)
-    conn.commit()
-    return cur.lastrowid
-
-
-def update_task(conn, task):
-    sql = ''' UPDATE tasks
-              SET desc = ?
-              WHERE id = ?'''
-    cur = conn.cursor()
-    cur.execute(sql, task)
-    conn.commit()
-
-
-def view_tasks(conn):
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM tasks")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-
-def delete_task(conn, taskID):
-    # Should probably reset taskIDs?  Not sure if that's a smart move
-    sql = 'DELETE FROM tasks WHERE id=?'
-    cur = conn.cursor()
-    cur.execute(sql, taskID)
-    conn.commit()
-
-
-def mark_task_as_done(conn, idAndText):
-    sql = ''' UPDATE tasks
-                  SET complete = ?
-                  WHERE id = ?'''
-    cur = conn.cursor()
-    cur.execute(sql, idAndText)
-    conn.commit()
+import tododatabaselayer
 
 
 def main():
-    database = r"C:\sqlite\db\todo.db"
-    conn = create_connection(database)
+    databasePath = r"C:\sqlite\db\todo.db"
+    database = tododatabaselayer.databaseLayer(databasePath)
 
     script = sys.argv[0]
     if len(sys.argv) == 1:  # no arguments, so print help message
@@ -72,27 +17,27 @@ def main():
         # By default, set complete column to incomplete
         desc = sys.argv[2]
         task = (desc, 'Incomplete')
-        new_task(conn, task)       # ???
+        database.new_task(database.conn, task)       # ???
 
     elif action == '--view':
         # Print rows
-        view_tasks(conn)
+        database.view_tasks(database.conn)
 
     elif action == '--edit':
         # Take ID to edit and new description
         taskID = sys.argv[2]
         desc = sys.argv[3]
-        update_task(conn, (desc, taskID,))
+        database.update_task(database.conn, (desc, taskID,))
 
     elif action == '--delete':
         taskID = sys.argv[2]
-        delete_task(conn, (taskID,))
+        database.delete_task(database.conn, (taskID,))
 
     elif action == '--mark':
         # Create markText variable because I don't know how else to create the tuple
         taskID = sys.argv[2]
         markText = "Done"
-        mark_task_as_done(conn, (markText, taskID))
+        database.mark_task_as_done(database.conn, (markText, taskID))
 
 
 if __name__ == '__main__':
---
+++ tododatabaselayer.py
@@ -0,0 +1,58 @@
+import sqlite3
+from sqlite3 import Error
+
+
+class databaseLayer:
+    def __init__(self, databasePath):
+        self.conn = self.create_connection(databasePath)
+
+    def create_connection(self, databasePath):
+        conn = None
+        try:
+            conn = sqlite3.connect(databasePath)
+        except Error as e:
+            print(e)
+        return conn
+
+    def new_task(self, conn, task):
+
+        # cursor object https://docs.python.org/2/library/sqlite3.html#sqlite3.Cursor
+
+        sql = ''' INSERT INTO tasks(desc, complete)
+                      VALUES(?,?) '''
+        cur = conn.cursor()
+        cur.execute(sql, task)
+        conn.commit()
+        return cur.lastrowid
+
+    def update_task(self, conn, task):
+        sql = ''' UPDATE tasks
+                  SET desc = ?
+                  WHERE id = ?'''
+        cur = conn.cursor()
+        cur.execute(sql, task)
+        conn.commit()
+
+    def view_tasks(self, conn):
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM tasks")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+    def delete_task(self, conn, taskID):
+        # Should probably reset taskIDs?  Not sure if that's a smart move
+        sql = 'DELETE FROM tasks WHERE id=?'
+        cur = conn.cursor()
+        cur.execute(sql, taskID)
+        conn.commit()
+
+    def mark_task_as_done(self, conn, idAndText):
+        sql = ''' UPDATE tasks
+                      SET complete = ?
+                      WHERE id = ?'''
+        cur = conn.cursor()
+        cur.execute(sql, idAndText)
+        conn.commit()
